d657a037fad8c0d3c1ec4d0429dd458127b05d1f
shaoping <260259423@qq.com>
2025-02-28 10:48:18 +0800

    Update pad_data_collator.py

--- internvl_chat/internvl/patch/pad_data_collator.py
+++ internvl_chat/internvl/patch/pad_data_collator.py
@@ -99,13 +99,16 @@ def concat_pad_data_collator(features, max_item_length=None, pad_id=0):
     # Again, we will use the first element to figure out which key/values are not None for this model.
     for k, v in first.items():
         if k not in ('label', 'label_ids', 'pixel_values', 'image_flags') and \
-                v is not None and not isinstance(v, str):
-            if isinstance(v, torch.Tensor):
-                batch[k] = torch.stack([f[k] for f in features])
-            elif isinstance(v, np.ndarray):
-                batch[k] = torch.tensor(np.stack([f[k] for f in features]))
+                v is not None:
+            if not isinstance(v, str):
+                if isinstance(v, torch.Tensor):
+                    batch[k] = torch.stack([f[k] for f in features])
+                elif isinstance(v, np.ndarray):
+                    batch[k] = torch.tensor(np.stack([f[k] for f in features]))
+                else:
+                    batch[k] = torch.tensor([f[k] for f in features])
             else:
-                batch[k] = torch.tensor([f[k] for f in features])
+                batch[k] = [f[k] for f in features]
         if k in ('pixel_values', 'image_flags'):
             if isinstance(v, torch.Tensor):
                 batch[k] = torch.concat([f[k] for f in features])
